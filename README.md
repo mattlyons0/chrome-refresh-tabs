7bd4e48c68414a67bf9353ce2284c3cc0733fb35
Matt Lyons <matt@matt-lyons.net>
2015-10-27 07:57:07 -0700

    Add script and docs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chrome-refresh-tabs
 Python script to refresh specific tabs in chrome/chromium. Useful in build scripts when working with webservers.
+
+## Installation
+Clone this repository.
+Install `websocket_client` for Python2 using either `pip install -r requirements.txt` from the cloned repository or `pip install websocket_client`. If you have multiple versions of Python installed you may need to use pip2 instead of pip.
+
+## Usage
+Start Chrome/Chromium with the flag: `--remote-debugging-port=9222` as this script uses the [Remote debugging protocol](https://developer.chrome.com/devtools/docs/debugger-protocol). (You can check if this is enabled by going to [http://localhost:9222](http://localhost:9222).
+Run chromeRefresh.py with python2. It can take the following optional arguments: `python2 chromeRefresh.py [queryString] [delay]` where [queryString] is a string contained in the web url in which pages will be refreshed and [delay] is a the delay before refreshing in seconds.
+
+Example: `python2 chromeRefresh.py 'youtube' 2`: Will refresh all tabs with the string 'youtube' in their URL after 2 seconds of delay.
---
+++ chromeRefresh.py
@@ -0,0 +1,47 @@
+#Originally from http://stackoverflow.com/questions/11344414/windows-chrome-refresh-tab-0or-current-tab-via-command-line
+#Documentation https://developer.chrome.com/devtools/docs/debugger-protocol#extension
+#Refreshes pages open in chrome which contain the first argument passed.
+#If there is a 2nd argument it will be parsed as the delay
+import urllib2
+import urllib
+import os
+import subprocess
+import json
+import sys
+import time
+
+from websocket import create_connection
+
+def refresh_page(url):
+    data = json.load(urllib2.urlopen('http://localhost:9222/json'))
+
+    found_page = False
+    for page in data:
+        if page['url'].find(url) != -1:
+            found_page = True
+            websocketURL = page['webSocketDebuggerUrl']
+            ws = create_connection(websocketURL)
+
+            obj = {  "id": 0,
+                     "method": "Page.reload",
+                     "params":
+                     {
+                       "ignoreCache": False,
+                       "scriptToEvaluateOnLoad": ""
+                     }
+                  }
+
+            dev_request = json.dumps(obj)
+            ws.send(dev_request)
+            result =  ws.recv()
+            ws.close()
+    if not found_page:
+        raise Exception("No page found")
+try:
+    time.sleep(float(sys.argv[2]))
+except:
+    pass #No 2nd arg passed, or it wasn't a number
+try:
+    refresh_page(sys.argv[1])
+except:
+    refresh_page(""); #Refresh all pages if no argument is specified
